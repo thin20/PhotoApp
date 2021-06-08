8da0b6975e184707256103530612d0c7e088308f
thin20 <70517637+thin20@users.noreply.github.com>
2021-06-08 17:31:26 +0700

    Add layout components: PhotoList and PhotoCard

--- src/features/Photo/components/PhotoList/index.jsx
+++ src/features/Photo/components/PhotoList/index.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import { Col, Container, Row } from 'reactstrap';
+import PropTypes from 'prop-types';
+import PhotoCard from '../PhotoCard';
 
 PhotoList.propTypes = {
-
+    photoList: PropTypes.array,
+    onPhotoEditClick: PropTypes.func,
+    onPhotoRemoveClick: PropTypes.func
 };
 
+PhotoList.defaultProps = {
+    photoList: [],
+    onPhotoEditClick: null,
+    onPhotoRemoveClick: null
+}
+
 function PhotoList(props) {
+    const { photoList, onPhotoEditClick, onPhotoRemoveClick } = props;
+
     return (
-        <div>
-            this is Photo List
-        </div>
+        <Container>
+            <Row>
+                {photoList.map((photo, index) => (
+                    <Col key={index} xs="12" sm="6" md="6" lg="3">
+                        <PhotoCard
+                            photo={photo} onEditClick={onPhotoEditClick} onRemoveClick={onPhotoRemoveClick}
+                        />
+                    </Col>
+                ))}
+            </Row>
+
+        </Container>
     );
 }
 
--- src/features/Photo/pages/MainPage/index.jsx
+++ src/features/Photo/pages/MainPage/index.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import Banner from 'components/Banner';
 import Images from 'constants/images';
 import { useSelector } from 'react-redux';
+import PhotoList from 'features/Photo/components/PhotoList';
 
 MainPage.propTypes = {
 
@@ -12,14 +13,27 @@ MainPage.propTypes = {
 function MainPage(props) {
     const photos = useSelector(state => state.photos);
     console.log('List of photos:', photos);
+
+    const handelPhotoEditClick = (photo) => {
+        console.log('Edit: ', photo)
+    }
+
+    const handelPhotoRemoveClick = (photo) => {
+        console.log('Remove: ', photo);
+    }
+
     return (
         <div className="photo-main">
             <Banner title="Your awesome photo" backgroundUrl={Images.PINK_BG} />
             <Container className="text-center">
                 <Link to="photos/add">Add New photo</Link>
             </Container>
+            <PhotoList
+                photoList={photos}
+                onPhotoEditClick={handelPhotoEditClick}
+                onPhotoRemoveClick={handelPhotoRemoveClick}
+            />
         </div>
-
     );
 }
 
